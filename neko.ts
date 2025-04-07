aaf666b371008656174ccf4da19ad95d8a3fd400
Jb Carreon <jbcarreon212@gmail.com>
2025-04-07 11:37:11 +0800

    change the jankness

--- neko.ts
+++ neko.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env bun
 
+import { styleText } from 'node:util';
 import { name, version, description } from './package.json';
-import { color } from 'console-log-colors';
 import * as commander from 'commander';
 
 function getCommandsAndOptions(cli: commander.Command) {
@@ -25,33 +25,34 @@ function getCommandsAndOptions(cli: commander.Command) {
 const cli = new commander.Command(name)
     .version(version);
 
+cli.showHelpAfterError(true);
+
+cli.configureHelp({
+    styleCommandText: (str) => styleText(['cyan', 'bold'], str),
+    styleCommandDescription: (str) => styleText('blue', str),
+    styleOptionText: (str) => styleText('gray', str),
+    styleArgumentText: (str) => styleText('blue', str),
+    styleSubcommandText: (str) => styleText(['blue', 'bold'], str),
+});
+
+cli.addHelpText(
+    'before',
+    `${description.replace(`${name} `, `${styleText(['blue', 'bold'], name)} `)} ${styleText('gray', `(${version})`)}\n`
+)
+
+cli.addHelpText(
+    'after',
+    '\nMade with love by the indiefellas team:\n  ' + styleText(['green', 'underline'], 'https://team.indieseas.net'),
+);
+
 cli.command('edit')
     .option('--editor', 'The executable of the editor of your choice (e.g. code for Visual Studio Code)')
     .option('--site', 'The site you want to create the session for (defaults to your site)')
     .description('Creates an interactive edit session, right in your IDE')
+    .action((command) => {
+        console.log(command.args)
+    })
 
-let { commands, options } = getCommandsAndOptions(cli);
-
-cli.helpInformation = function () {
-    return `${description.replace(`${name} `, `${color.blue(color.bold(name))} `)} (${color.gray(version)})
-
-Usage: ${color.bold(`neko ${color.blue('<command>')} ${color.gray('[...options]')}`)}
-${commands.length > 0 ? '\nCommands:\n' + 
-    commands.map((c) => 
-        `  ${color.blue(color.bold(c.name))}${" ".repeat(20 -c.name.length)}${c.description}${c.options.length > 0 ?
-        `\n${c.options.map((o) => `    ${o.flags.split(', ').map((s) => 
-                color.gray(color.bold(s))).join(', ')}${" ".repeat(18 -o.flags.length)}${o.description}`
-            ).join('\n')}` : ''}`
-    ).join('\n'):
-''}
-${options.length > 0 ? '\nOptions:\n' +
-    options.map((o) => `  ${o.flags.split(', ').map((s) => 
-        color.gray(color.bold(s))).join(', ')}${" ".repeat(20 -o.flags.length)}${o.description}`
-    ).join('\n'):
-''}
-
-Made with love by the indiefellas team:
-   ${color.green('https://team.indieseas.net')}
-`};
 
 cli.parse(process.argv)
+
